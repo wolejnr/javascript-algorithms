7a80bcffff5bbc7500e96829cedbea0f0e0b9481
wolejnr <wolejr@gmail.com>
2018-07-01 23:48:36 +0100

    The working solution to the Finders Keepers problem

--- findElement.js
+++ findElement.js
@@ -1,16 +1,13 @@
 function findElement(arr, func) {
-    //let num = 0;
-    function func(arr){
-      for(let i=0; i<arr.length; i++){
-        if(arr[i] % 2 === 0) {
-          return arr[i];
-          //break;
-        }
-        else {
-          return undefined;
-        }
-      }
-    };
+    let num = 0;
+    
+    for(let i=0; i<arr.length; i++){
+      if(func(arr[i]) === true){
+        num = arr[i];
+        return num;
+       }
+    }
+    
 }
 
 console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }));
